d9d9280546de0f1257dc5dfa6226ff3ee5522125
AmanPatel1521 <amanpatel335@gmail.com>
2025-07-21 18:04:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -165,7 +165,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function connectWebSocket() {
-        socket = io("http://127.0.0.1:5000");
+        socket = io("https://sudoku-multi-backend.onrender.com");
 
         socket.on('connect', () => {
             console.log('Socket.IO connected!');
